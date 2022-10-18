7c38a248aad1f5bded5197e73871b7f32a67813c
Yulia <yulia1201556@gmail.com>
2022-10-18 17:16:30 +0300

    Список из n чисел определённой последовательности

--- Task/003.py
+++ Task/003.py
@@ -1,9 +1,21 @@
-# 3. Задайте список из n чисел последовательности 
+# Задайте список из n чисел последовательности 
 # (1+1/n)^n и выведите на экран их сумму.
 
-print ("Введите размер списка num: ")
-num = int(input())
-  
-list = [round((1+1/i)**i, 3) for i in range(1, num+1)]
+# list = []
+# result = 0
+# num = int(input("Введите размер списка: "))
+# if num <= 0:
+#     print("Error")
+# else:
+#     for i in range(1, num + 1):
+#         numbers = (round((1 + 1/i) ** i))
+#         list.append(numbers)
+#         result += numbers
+#     print(list)
+#     print(result)
 
-print(f"{list} \nСумма: {round(sum(list), 3)}")
+
+
+num = int(input("Введите размер списка: "))
+list = [round((1+1/i)**i) for i in range(1, num+1)]
+print(f"{list} \nСумма: {round(sum(list))}")
